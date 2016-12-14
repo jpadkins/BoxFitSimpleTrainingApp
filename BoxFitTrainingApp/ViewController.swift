202c4351d307582964da6bfa5cabfcf80f41f26b
jacobpadkins <jpadkins@smu.edu>
2016-12-14 01:24:20 -0600

    Initial Commit

---
+++ BoxFitTrainingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoxFitTrainingApp
+//
+//  Created by Jacob Adkins on 12/14/16.
+//  Copyright © 2016 Jacob Adkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
